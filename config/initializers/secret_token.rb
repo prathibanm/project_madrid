1fd84e4646065d3055b49d10504e77119abcddaf
prathibanm <prathiban.m85@gmail.com>
2012-06-26 00:09:12 +0530

    first cut

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectMadrid::Application.config.secret_token = 'b7b57cb0726a3563a8a8116800440a3d4cb9e9c801402c1883c25f303f55a0c1717a9b0a3f07fcd862c2bc39de5efad743307bc3c604624c216e04cef817da64'
